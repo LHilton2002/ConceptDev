f937f6f67db5df1ea02cdf48c304fcc2ad6aac4f
Wxrlockz <72805699+Wxrlockz@users.noreply.github.com>
2023-01-04 07:04:24 +0000

    Commit
    Commit

---
+++ Assets/scripts/DontStopAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontStopAudio : MonoBehaviour
+{
+    public static DontStopAudio BGInstance;
+
+
+    void Awake()
+    {
+        if(BGInstance != null && BGInstance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        BGInstance = this;
+        DontDestroyOnLoad(this);
+
+
+
+        //DontDestroyOnLoad(transform.gameObject);
+    }
+
+}
+
+
+
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+    public static GameManager Instance;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countdownPage;
+    public Text scoreText;
+
+    public CameraShake cameraShake;
+
+
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver,
+        Countdown
+    }
+
+    int score = 0;
+    bool gameOver = true;
+
+    public bool GameOver { get { return gameOver; } }
+    public int Score { get { return score;  } }
+
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void OnEnable()
+    {
+        CountdownText.OnCountdownFinished += OnCountdownFinished;
+        TapController.OnPlayerDied += OnPlayerDied;
+        TapController.OnPlayerScored += OnPlayerScored;
+    }
+
+    void OnDisable()
+    {
+        CountdownText.OnCountdownFinished -= OnCountdownFinished;
+        TapController.OnPlayerDied -= OnPlayerDied;
+        TapController.OnPlayerScored -= OnPlayerScored;
+    }
+
+    void OnCountdownFinished()
+    {
+        SetPageState(PageState.None);
+        OnGameStarted(); // sent to tapcontroller
+        score = 0;
+        gameOver = false;
+    }
+
+    void OnPlayerDied()
+    {
+        gameOver = true;
+        int savedScore = PlayerPrefs.GetInt("HighScore");
+        if (score > savedScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+        SetPageState(PageState.GameOver);
+    }
+
+    void OnPlayerScored()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.Countdown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(true);
+                break;
+        }
+    }
+
+    public void ConfirmGameOver()
+    {
+        // when replay button hit
+        OnGameOverConfirmed(); // event sent tapcontroller
+        scoreText.text = "0";
+        SetPageState(PageState.Start);
+
+    }
+
+    public void StartGame()
+    {
+        // when play button hit
+        SetPageState(PageState.Countdown);
+    }
+
+
+
+    void Update()
+    {
+        if (score == 0)
+        {
+            Time.timeScale = 1f;
+
+        }
+
+
+        if (score == 15) {
+            Time.timeScale = 1f;
+
+        }
+
+        if (score == 30)
+        {
+            Time.timeScale = 1f;
+
+        }
+
+        if (score == 45)
+        {
+            Time.timeScale = 1f;
+
+        }
+
+        if (score == 60)
+        {
+            Time.timeScale = 1f;
+
+        }
+
+        if (score == 75)
+        {
+            Time.timeScale = 1f;
+
+        }
+    }
+}
